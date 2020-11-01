e83f827b33029bb54d1f1f360f86b25900210fb0
ljb <1641222384@qq.com>
2020-11-02 01:55:39 +0800

    1

--- cloud-provider-pament8001/src/main/java/com/atguigu/PaMentMain8001.java
+++ cloud-provider-pament8001/src/main/java/com/atguigu/PaMentMain8001.java
@@ -2,6 +2,7 @@ package com.atguigu;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -14,4 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @ComponentScan(basePackages = "com.atguigu")
 public class PaMentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaMentMain8001.class,args);
+    }
 }
